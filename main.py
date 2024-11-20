8a2b4fbb4565b0fa83d1ae286e6a2151cc73449a
612284 <612284@gmail.com>
2024-11-20 11:44:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 def is_prime(n):
     """Перевіряє, чи є число простим."""
@@ -12,3 +13,10 @@ def is_prime(n):
 def count_primes_in_range(start, end):
     """Рахує кількість простих чисел у заданому діапазоні."""
     return sum(1 for i in range(start, end + 1) if is_prime(i))
+
+def simulate_heavy_computation(n):
+    """Імітує важкий розрахунок, наприклад, обчислення факторіалу."""
+    total = 1
+    for i in range(1, n + 1):
+        total *= i
+    return total
